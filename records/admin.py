b1fbf328b3e67ae56ee565c51480c19123393676
Aminu Saminu Khamees <ask4all1@gmail.com>
2018-12-12 19:14:03 +0100

    Setting up Models

--- records/admin.py
+++ records/admin.py
@@ -1,3 +1,9 @@
 from django.contrib import admin
 
 # Register your models here.
+from .models import Cardiology, Endocrinology
+
+
+admin.site.register(Cardiology)
+admin.site.register(Endocrinology)
+
--- records/models.py
+++ records/models.py
@@ -1,3 +1,11 @@
 from django.db import models
+from django.utils import timezone
 
 # Create your models here.
+
+class Cardiology(models.Model):
+	title = models.CharField(max_length=255, help_text="Enter Patient Name")
+
+class Endocrinology(models.Model):
+	diagonosis = models.CharField(max_length=255)
+	time = models.DateField(default = timezone.now)
