ca90a1cff82e5bb4d953c36c4ddead506b0c38ea
Aminu Saminu Khamees <ask4all1@gmail.com>
2018-12-15 17:44:47 +0100

    Admin can now log in from homepage

--- hospital/settings.py
+++ hospital/settings.py
@@ -55,7 +55,7 @@ ROOT_URLCONF = 'hospital.urls'
 TEMPLATES = [
     {
         'BACKEND': 'django.template.backends.django.DjangoTemplates',
-        'DIRS': [os.path.join(BASE_DIR, 'templates')],
+        'DIRS': [],
         'APP_DIRS': True,
         'OPTIONS': {
             'context_processors': [
@@ -119,5 +119,3 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/2.1/howto/static-files/
 
 STATIC_URL = '/static/'
-LOGIN_REDIRECT_URL = '/accounts/validation.html'
-LOGIN_URL = '/accounts/'
